cda1af56fc0d2055376a48638de8c5d29600c0df
CosmusMutuku <100933366+CosmusMutuku@users.noreply.github.com>
2022-05-21 22:56:34 +0300

    Update model.py

--- model.py
+++ model.py
@@ -29,6 +29,46 @@ import json
 
 def _preprocess_data(data):
     """Private helper function to preprocess data for model prediction.
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+train_data = pd.read_csv('df_train.csv') # load the train data
+test_data = pd.read_csv('df_test.csv')  # load the test data
+data=[train_data, test_data]
+df=pd.concat(data)
+df['time'] = pd.to_datetime(df['time'])
+# extract relevant metrics like year, month, weekday, hour from the time feature
+df['Year'] = df['time'].dt.year
+# extract month
+df['Month'] = df['time'].dt.month
+# extract weekday
+df['Weekday'] = df['time'].dt.dayofweek
+# extract hour from the time feature
+df['Hour'] = df['time'].dt.hour
+df.drop("time", axis=1, inplace=True)
+df['Valencia_wind_deg'] = df['Valencia_wind_deg'].str.extract(r'(\d+$)')
+df["Valencia_wind_deg"] = pd.to_numeric(df["Valencia_wind_deg"])
+df['Seville_pressure'] = df['Seville_pressure'].str.extract(r'(\d+$)')
+df["Seville_pressure"] = pd.to_numeric(df["Seville_pressure"])
+df["Valencia_pressure"].fillna(train_data["Valencia_pressure"].mode()[0], inplace=True)
+ready_test_df = df[df["load_shortfall_3h"].isnull()]
+ready_test_df = ready_test_df.drop("load_shortfall_3h", axis=1)
+ready_train_df = df[df["load_shortfall_3h"].notnull()]
+X = ready_train_df.drop(columns = 'load_shortfall_3h')
+y = ready_train_df['load_shortfall_3h'].astype('int')
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+ 
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+lm = LinearRegression()
+#Fit the model into training set
+lm.fit(X_train, y_train)
+
+#predict on unseen data
+predict = lm.predict(X_test)
+
 
     NB: If you have utilised feature engineering/selection in order to create
     your final model you will need to define the code here.
